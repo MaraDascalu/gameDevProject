8d98b0a3de773ff87bd5e74025a66ca2d30cf858
alexbrinza2001 <tudor.brinza@s.unibuc.ro>
2022-12-03 17:48:09 +0200

    Sprint3_3

--- Assets/Scripts/Environment/GenerateLevel.cs
+++ Assets/Scripts/Environment/GenerateLevel.cs
@@ -14,6 +14,7 @@ public class GenerateLevel : MonoBehaviour
         {
             sectionCreation = true;
             StartCoroutine(GenerateSection());
+           // StartCoroutine(DeleteSection());
         }
     }
 
@@ -25,4 +26,13 @@ public class GenerateLevel : MonoBehaviour
         yield return new WaitForSeconds(2);
         sectionCreation = false;
     }
+
+    /*IEnumerator DeleteSection()
+    {
+        sectionNumber = Random.Range(0, 2);
+        Instantiate(section[sectionNumber], new Vector3(0, 0, position), Quaternion.identity);
+        position += 100;
+        yield return new WaitForSeconds(2);
+        sectionCreation = false;
+    }*/
 }
