d5e9d08a7b17e5bea5215ce5c1ff388bd32bbcbe
alexbrinza2001 <tudor.brinza@s.unibuc.ro>
2023-01-13 03:20:34 +0200

    second map update

--- Assets/Scripts/Collectables/CoffeeCollect.cs
+++ Assets/Scripts/Collectables/CoffeeCollect.cs
@@ -1,24 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading;
+using System;
 
 public class CoffeeCollect : MonoBehaviour
 {
     public AudioSource coffeeSound;
 
+    public void Wait()
+    {
+        StartCoroutine(_wait());
+    }
+
+    IEnumerator _wait()
+    {
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        yield return new WaitForSeconds(5);
+        PlayerMovement._forwardSpeed = 10.0f;
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        Debug.Log(PlayerMovement._forwardSpeed);
+    }
+
     void OnTriggerEnter(Collider other)
-    {   
+    {
         Debug.Log("Coffee sound");
         coffeeSound.Play();
-        this.gameObject.SetActive(false);
-        PlayerMovement._forwardSpeed += 0.5f;
+        this.gameObject.SetActive(true);
+        PlayerMovement._forwardSpeed += 20.0f;
         Debug.Log("PlayerSpeed: " + PlayerMovement._forwardSpeed);
-        // yield return new WaitForSeconds(5f);
-        // PlayerMovement._forwardSpeed = 10f;
-        // Debug.Log(PlayerMovement._forwardSpeed);
-        // this.gameObject.SetActive(true);
+        Wait();
+        Debug.Log(PlayerMovement._forwardSpeed);
 
     }
-     
+
 }
