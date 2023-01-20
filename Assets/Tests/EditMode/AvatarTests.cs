35e6daeda7d07fd3d7dc120805abc6d3379c813b
Miruna Lupascu <mirunastefania2001@gmail.com>
2023-01-20 22:37:16 +0200

    UnitTests

---
+++ Assets/Tests/EditMode/AvatarTests.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class AvatarTests
+{
+    [SetUp]
+    public void SetUp() {
+        var gameObject = new GameObject();
+    }
+
+    [Test]
+    public void Start_SetsCorrectInitialAvatar() {
+    }
+    
+    [Test]
+    public void ChangeAvatar_ChangesActiveAvatar() {
+
+    }
+
+    [Test]
+    public void ChangeAvatar_WrapsAroundList() {
+    }
+
+     [Test]
+    public void PlayNewGame_SavesSelectedAvatar() {
+    }
+
+    [Test]
+    public void PlayNewGame_LoadsLevelPickerScene() {
+
+    }
+}
